d755c95d9102364f8597f357f905f9f265c23bca
honeytech <honey93k@gmail.com>
2023-01-03 15:52:50 -0800

    added my branch

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-// const helpers = require("./utils/helpers");
+//. const helpers = require("./utils/helpers");
 
 const sequelize = require("./config");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
