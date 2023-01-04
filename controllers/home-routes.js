739745977eb5fa4ab17d9e8f17244b06a381a4c3
jthnguyen9909 <jthnguyen9909@gmail.com>
2023-01-04 14:04:36 -0800

    merged branch with updated game_logic

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,37 +1,37 @@
-const router = require('express').Router();
-const { User } = require('../models/');
+const router = require("express").Router();
+const { User } = require("../models/");
 
-router.get('/', async (req, res) => {
-    try {
-        res.render('main');
-    } catch(err) {
-        res.status(500).json(err);
-    }
+router.get("/", async (req, res) => {
+  try {
+    res.render("main");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/login', (req, res) => {
-    if(req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    res.render('login');
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
 });
 
-router.get('/signup', (req, res) => {
-    if(req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    res.render('signup');
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
 });
 
 // high score page for only when user is logged in
-router.get('/highscore', withAuth, async (req, res) => {
-    try {
-        res.render('highscores');
-    } catch (err) {
-        res.redirect('login');
-    }
+router.get("/highscore", withAuth, async (req, res) => {
+  try {
+    res.render("highscores");
+  } catch (err) {
+    res.redirect("login");
+  }
 });
 
 module.exports = router;
