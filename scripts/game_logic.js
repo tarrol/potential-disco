75188d15e428edb1e1d3854993050a2cac824749
hynookim98 <hynookim98@gmail.com>
2023-01-04 13:35:43 -0800

    switched game logic to check win codition with coordinates | cleaned up code

--- scripts/game_logic.js
+++ scripts/game_logic.js
@@ -1,130 +1,128 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const squares = document.querySelectorAll(".grid div");
-  const result = document.querySelector("#result");
-  const noti = document.querySelector("#nortification");
-  const displayCurrentPlayer = document.querySelector("#current-player");
-  let currentPlayer = 1;
+var playerRed = "R";
+var playerYellow = "Y";
+var currPlayer = playerRed;
 
-  const winningArrays = [
-    [0, 1, 2, 3],
-    [41, 40, 39, 38],
-    [7, 8, 9, 10],
-    [34, 33, 32, 31],
-    [14, 15, 16, 17],
-    [27, 26, 25, 24],
-    [21, 22, 23, 24],
-    [20, 19, 18, 17],
-    [28, 29, 30, 31],
-    [13, 12, 11, 10],
-    [35, 36, 37, 38],
-    [6, 5, 4, 3],
-    [0, 7, 14, 21],
-    [41, 34, 27, 20],
-    [1, 8, 15, 22],
-    [40, 33, 26, 19],
-    [2, 9, 16, 23],
-    [39, 32, 25, 18],
-    [3, 10, 17, 24],
-    [38, 31, 24, 17],
-    [4, 11, 18, 25],
-    [37, 30, 23, 16],
-    [5, 12, 19, 26],
-    [36, 29, 22, 15],
-    [6, 13, 20, 27],
-    [35, 28, 21, 14],
-    [0, 8, 16, 24],
-    [41, 33, 25, 17],
-    [7, 15, 23, 31],
-    [34, 26, 18, 10],
-    [14, 22, 30, 38],
-    [27, 19, 11, 3],
-    [35, 29, 23, 17],
-    [6, 12, 18, 24],
-    [28, 22, 16, 10],
-    [13, 19, 25, 31],
-    [21, 15, 9, 3],
-    [20, 26, 32, 38],
-    [36, 30, 24, 18],
-    [5, 11, 17, 23],
-    [37, 31, 25, 19],
-    [4, 10, 16, 22],
-    [2, 10, 18, 26],
-    [39, 31, 23, 15],
-    [1, 9, 17, 25],
-    [40, 32, 24, 16],
-    [9, 17, 25, 33],
-    [8, 16, 24, 32],
-    [11, 17, 23, 29],
-    [12, 18, 24, 30],
-    [1, 2, 3, 4],
-    [5, 4, 3, 2],
-    [8, 9, 10, 11],
-    [12, 11, 10, 9],
-    [15, 16, 17, 18],
-    [19, 18, 17, 16],
-    [22, 23, 24, 25],
-    [26, 25, 24, 23],
-    [29, 30, 31, 32],
-    [33, 32, 31, 30],
-    [36, 37, 38, 39],
-    [40, 39, 38, 37],
-    [7, 14, 21, 28],
-    [8, 15, 22, 29],
-    [9, 16, 23, 30],
-    [10, 17, 24, 31],
-    [11, 18, 25, 32],
-    [12, 19, 26, 33],
-    [13, 20, 27, 34],
-  ];
+var gameOver = false;
+var board;
 
-  function checkBoard() {
-    for (let y = 0; y < winningArrays.length; y++) {
-      const square1 = squares[winningArrays[y][0]];
-      const square2 = squares[winningArrays[y][1]];
-      const square3 = squares[winningArrays[y][2]];
-      const square4 = squares[winningArrays[y][3]];
+var rows = 6;
+var columns = 7;
+var currColumns = []; //keeps track of which row each column is at.
 
-      if (
-        square1.classList.contains("player-one") &&
-        square2.classList.contains("player-one") &&
-        square3.classList.contains("player-one") &&
-        square4.classList.contains("player-one")
-      ) {
-        result.innerHTML = "Player One Wins!";
-      }
-      if (
-        square1.classList.contains("player-two") &&
-        square2.classList.contains("player-two") &&
-        square3.classList.contains("player-two") &&
-        square4.classList.contains("player-two")
-      ) {
-        result.innerHTML = "Player Two Wins!";
-      }
-    }
-  }
+window.onload = function() {
+    setGame();
+}
 
-  for (let i = 0; i < squares.length; i++) {
-    squares[i].onclick = () => {
-      //clear nortification
-      noti.innerHTML = "";
-      if (
-        squares[i + 7].classList.contains("taken") &&
-        !squares[i].classList.contains("taken")
-      ) {
-        if (currentPlayer == 1) {
-          squares[i].classList.add("taken");
-          squares[i].classList.add("player-one");
-          currentPlayer = 2;
-          displayCurrentPlayer.innerHTML = currentPlayer;
-        } else if (currentPlayer == 2) {
-          squares[i].classList.add("taken");
-          squares[i].classList.add("player-two");
-          currentPlayer = 1;
-          displayCurrentPlayer.innerHTML = currentPlayer;
+function setGame() {
+    board = [];
+    currColumns = [5, 5, 5, 5, 5, 5, 5];
+
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            // JS
+            row.push(' ');
+            // HTML
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click", setPiece);
+            document.getElementById("board").append(tile);
         }
-        //add noti
-      } else noti.innerHTML = "You can't go here, please choose another one";
-      checkBoard();
-    };
-  }
-});
+        board.push(row);
+    }
+}
+
+function setPiece() {
+    if (gameOver) {
+        return;
+    }
+
+    //get coords of that tile clicked
+    let coords = this.id.split("-");
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    // figure out which row the current column should be on
+    r = currColumns[c]; 
+
+    if (r < 0) { // board[r][c] != ' '
+        return;
+    }
+
+    board[r][c] = currPlayer; //update JS board
+    let tile = document.getElementById(r.toString() + "-" + c.toString());
+    if (currPlayer == playerRed) {
+        tile.classList.add("red-piece");
+        currPlayer = playerYellow;
+    }
+    else {
+        tile.classList.add("yellow-piece");
+        currPlayer = playerRed;
+    }
+
+    r -= 1; //update the row height for that column
+    currColumns[c] = r; //update the array
+
+    checkWinner();
+}
+
+function checkWinner() {
+     // horizontal
+     for (let r = 0; r < rows; r++) {
+         for (let c = 0; c < columns - 3; c++){
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+         }
+    }
+
+    // vertical
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 3; r++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    // anti diagonal
+    for (let r = 0; r < rows - 3; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    // diagonal
+    for (let r = 3; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+function setWinner(r, c) {
+    let winner = document.getElementById("winner");
+    if (board[r][c] == playerRed) {
+        winner.innerText = "Red Wins";             
+    } else {
+        winner.innerText = "Yellow Wins";
+    }
+    gameOver = true;
+}
