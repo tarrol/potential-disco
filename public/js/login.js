a19c5538e627120df8da2b8d2bd29c9ea1ae3eca
hynookim98 <hynookim98@gmail.com>
2023-01-06 11:29:00 -0800

    routes

--- public/js/login.js
+++ public/js/login.js
@@ -5,15 +5,15 @@ const loginFormHandler = async function (event) {
   const usernameEl = document.querySelector("#username-login");
   const passwordEl = document.querySelector("#password-login");
 
-  const response = await fetch("/api/users/login", {
-    method: "POST",
-    body: JSON.stringify({
-      username: usernameEl.value,
-      password: passwordEl.value,
-    }),
-    // ?
-    headers: { "Content-Type": "application/json" },
-  });
+    const response =  await fetch ('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: usernameEl.value,
+            password: passwordEl.value,
+        }),
+        // ? 
+        headers: { 'Content-Type': 'application/json' },
+    });
 
   if (response.ok) {
     document.location.replace("/");
--- public/js/signup.js
+++ public/js/signup.js
@@ -4,14 +4,14 @@ const signupFormHandler = async function (event) {
   const usernameEl = document.querySelector("#username-signup");
   const passwordEl = document.querySelector("#password-signup");
 
-  const response = await fetch("/api/users", {
-    method: "POST",
-    body: JSON.stringify({
-      username: usernameEl.value,
-      password: passwordEl.value,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
+    const response = await fetch('/api/users', {
+        method: "POST",
+        body: JSON.stringify({
+            username: usernameEl.value,
+            password: passwordEl.value,
+        }),
+        headers: { 'Content-Type': 'application/json'},
+    });
 
   if (response.ok) {
     document.location.replace("/");
