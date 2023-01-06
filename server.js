00b733df99094791bc856c37b72e721172366f20
jthnguyen9909 <jthnguyen9909@gmail.com>
2023-01-06 10:44:44 -0800

    handlebars changes

--- server.js
+++ server.js
@@ -45,17 +45,16 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", require("./controllers"));
 
 // Handlebars setting
-app.engine("handlebars", hbs.engine);
 app.set("view engine", "hbs");
-// app.engine(
-//   "hbs",
-//   exphbs.engine({
-//     extname: "hbs",
-//     defaultLayout: "index",
-//     layoutsDir: __dirname + "/views/layouts",
-//     partialsDir: __dirname + "/views/partials",
-//   })
-// );
+app.engine(
+  "hbs",
+  exphbs.engine({
+    extname: "hbs",
+    defaultLayout: "index",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+  })
+);
 
 // commented out for testing socket connection
 // app.get("/", (req, res) => {
