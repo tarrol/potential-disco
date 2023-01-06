0a346c2dc7d45527c3b486cdf467831f1fb095b2
hynookim98 <hynookim98@gmail.com>
2023-01-06 14:54:10 -0800

    started working on avatar

--- public/js/avatar.js
+++ public/js/avatar.js
@@ -1,4 +1,6 @@
 const avatarUrl = "https://avatars.dicebear.com/api/bottts/";
+const avatarUrlEnd = ".svg";
+const session = require('express-session')
 
 function getRandomAvatar(length) {
   let haystack =
@@ -15,5 +17,13 @@ function getRandomAvatar(length) {
   console.log(generatedAvatar);
   return generatedAvatar;
 }
+
+var image = new Image();
+image.src = req.session.avatarurl;
+
+document
+  .querySelector('#avatar-section')
+  .appendChild(image)
+  .addEventListener('submit', getRandomAvatar(8));
 
 module.exports = getRandomAvatar;
