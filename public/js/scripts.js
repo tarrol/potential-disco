589630bd52fbd01efd6fa70bb2e49e8e1f9d0ddb
jthnguyen9909 <jthnguyen9909@gmail.com>
2023-01-08 05:03:36 -0800

    changed game logic to work easier with socketio

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,23 +1,18 @@
-$(function () {
-  var socket = io.connect(),
+$(() => {
+  let socket = io.connect(),
     player = {},
-    yc = $(".your_color"),
+    mc = $(".my_color"),
     oc = $(".opponent_color"),
     your_turn = false,
     url = window.location.href.split("/"),
     room = url[url.length - 1];
 
-  var text = {
-    yt: "Your turn",
-    nyt: "Waiting for opponent",
-    popover_h2: "Waiting for opponent",
-    popover_p: "Give the url to a friend to play a game",
-    popover_h2_win: "You won the game!",
-    popover_p_win: "Give the url to a friend to play another game",
-    popover_h2_lose: "You lost the game..",
-    popover_p_lose: "Give the url to a friend to play another game",
-    popover_h2_draw: "Its a draw.. bummer!",
-    popover_p_draw: "Give the url to a friend to play another game",
+  let text = {
+    my_turn: "Your turn",
+    opponent_turn: "Waiting for opponent",
+    win: "You win!",
+    lose: "You lost.. Try harder next time.",
+    draw: "Its a draw. Both of you are losers..",
   };
 
   init();
@@ -26,63 +21,41 @@ $(function () {
     player.pid = data.pid;
     player.hash = data.hash;
     if (player.pid == "1") {
-      yc.addClass("red");
+      mc.addClass("red");
       oc.addClass("yellow");
       player.color = "red";
-      player.oponend = "yellow";
-      $(".underlay").removeClass("hidden");
-      $(".popover").removeClass("hidden");
+      player.oppColor = "yellow";
     } else {
-      $(".status").html(text.nyt);
-      yc.addClass("yellow");
+      $(".status").html(text.opponent_turn);
+      mc.addClass("yellow");
       oc.addClass("red");
-      oc.addClass("show");
+      oc.addClass("display");
       player.color = "yellow";
-      player.oponend = "red";
+      player.oppColor = "red";
     }
   });
 
   socket.on("winner", function (data) {
-    oc.removeClass("show");
-    yc.removeClass("show");
+    oc.removeClass("display");
+    mc.removeClass("display");
     change_turn(false);
-    for (var i = 0; i < 4; i++) {
-      $(".cols .col .coin#coin_" + data.winner.winner_coins[i]).addClass(
-        "winner_coin"
-      );
-    }
-
     if (data.winner.winner == player.pid) {
-      $(".popover h2").html(text.popover_h2_win);
-      $(".popover p").html(text.popover_p_win);
+      $(".status").html(text.win);
     } else {
-      $(".popover h2").html(text.popover_h2_lose);
-      $(".popover p").html(text.popover_p_lose);
+      $(".status").html(text.lose);
     }
-
-    setTimeout(function () {
-      $(".underlay").removeClass("hidden");
-      $(".popover").removeClass("hidden");
-    }, 2000);
   });
 
   socket.on("draw", function () {
-    oc.removeClass("show");
-    yc.removeClass("show");
+    oc.removeClass("display");
+    mc.removeClass("display");
     change_turn(false);
-    $(".popover h2").html(text.popover_h2_draw);
-    $(".popover p").html(text.popover_p_draw);
-    setTimeout(function () {
-      $(".underlay").removeClass("hidden");
-      $(".popover").removeClass("hidden");
-    }, 2000);
+    $(".status").html(text.draw);
   });
 
   socket.on("start", function (data) {
     change_turn(true);
-    yc.addClass("show");
-    $(".underlay").addClass("hidden");
-    $(".popover").addClass("hidden");
+    mc.addClass("display");
   });
 
   socket.on("stop", function (data) {
@@ -91,10 +64,10 @@ $(function () {
   });
 
   socket.on("move_made", function (data) {
-    make_move(data.col + 1, true);
+    setPiece(data.col + 1, true);
     change_turn(true);
-    yc.addClass("show");
-    oc.removeClass("show");
+    mc.addClass("display");
+    oc.removeClass("display");
   });
 
   socket.on("opponent_move", function (data) {
@@ -104,33 +77,31 @@ $(function () {
     console.debug(data);
   });
 
-  $(".cols > .col").mouseenter(function () {
-    if (your_turn) {
-      yc.css("left", $(this).index() * 100);
-      socket.emit("my_move", { col: $(this).index() });
-    }
-  });
-
   $(".cols > .col").click(function () {
     if (parseInt($(this).attr("data-in-col")) < 6) {
       if (your_turn) {
-        var col = $(this).index() + 1;
-        make_move(col);
-        socket.emit("makeMove", { col: col - 1, hash: player.hash });
+        let col = $(this).index() + 1;
+        setPiece(col);
+        socket.emit("setPiece", { col: col - 1, hash: player.hash });
         change_turn(false);
-        yc.removeClass("show");
-        oc.addClass("show");
+        mc.removeClass("display");
+        oc.addClass("display");
       }
     }
   });
 
-  function make_move(col, other) {
+  function init() {
+    socket.emit("join", { room: room });
+    $(".status").html("Give the url to a friend to play!");
+  }
+
+  function setPiece(col, other) {
     if (!other) other = false;
-    var col_elm = $(".cols > .col#col_" + col);
-    var current_in_col = parseInt(col_elm.attr("data-in-col"));
+    let col_elm = $(".cols > .col#col_" + col);
+    let current_in_col = parseInt(col_elm.attr("data-in-col"));
     col_elm.attr("data-in-col", current_in_col + 1);
-    var color = other ? player.oponend : player.color;
-    var new_coin = $(
+    let color = other ? player.oppColor : player.color;
+    let new_coin = $(
       '<div class="coin ' +
         color +
         '" id="coin_' +
@@ -144,37 +115,26 @@ $(function () {
       {
         top: 100 * (4 - current_in_col + 1),
       },
-      400
+      0
     );
   }
 
-  function init() {
-    socket.emit("join", { room: room });
-    $(".popover input").val(window.location.href);
-    $(".popover h2").html(text.popover_h2);
-    $(".popover p").html(text.popover_p);
-    $(".status").html("");
+  function change_turn(my_turn) {
+    if (my_turn) {
+      your_turn = true;
+      $(".status").html(text.my_turn);
+    } else {
+      your_turn = false;
+      $(".status").html(text.opponent_turn);
+    }
   }
 
   function reset_board() {
     $(".cols .col").attr("data-in-col", "0").html("");
-    yc.removeClass("yellow red");
+    mc.removeClass("yellow red");
     oc.removeClass("yellow red");
-    yc.removeClass("show");
-    oc.removeClass("show");
+    mc.removeClass("display");
+    oc.removeClass("display");
+    $(".status").html("");
   }
-
-  function change_turn(yt) {
-    if (yt) {
-      your_turn = true;
-      $(".status").html(text.yt);
-    } else {
-      your_turn = false;
-      $(".status").html(text.nyt);
-    }
-  }
-
-  $(".popover input").click(function () {
-    $(this).select();
-  });
 });
